4e09679e7416c50b47defcb1e78dd9d7553d4ac7
kavyasribathala-byte <kavyasribathala@gmail.com>
2026-02-07 12:19:37 +0530

    Add files via upload

---
+++ primitive.java
@@ -0,0 +1,20 @@
+public class primitive{
+    public static void main (String[] args) {
+        byte b = 1;
+        short a = 169;
+        int c = 100000;
+        long l = 12345678;
+        float f = 12.67f;
+        double de = 123.8956;
+        char cha = '$';
+        boolean value = true;
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(de);
+        System.out.println(cha);
+
+    }
+}
