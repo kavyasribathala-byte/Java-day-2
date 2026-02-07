09762adf24e867b5e3bb1ca278ff38e28c76f73f
kavyasribathala-byte <kavyasribathala@gmail.com>
2026-02-07 14:29:15 +0530

    Add files via upload

---
+++ lol.java
@@ -0,0 +1,9 @@
+public class Lol {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 07;
+        int c = 0x5e;
+        int d = 0b100;
+        System.out.println(b);
+    }
+}
---
+++ opera.java
@@ -0,0 +1,21 @@
+class opera {
+    public static void main (String[] args){
+        int a = 8;
+        int b = 4;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a & b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+        System.out.println(a * b);
+        System.out.println(a--);
+        System.out.println(a++);
+        System.out.println(a += b);
+        System.out.println(a -= b);
+        System.out.println(a = b);
+        System.out.println(a ^ b);
+    }
+}
