a6bdbf9b16ab1a231391b9d750710ad766770b80
kavyasribathala-byte <kavyasribathala@gmail.com>
2026-02-07 14:40:44 +0530

    Add files via upload

---
+++ marks.java
@@ -0,0 +1,12 @@
+public class Marks {
+    public static void main (String[] args) {
+        int sanskrit = 99;
+        int maths = 87;
+        int physics = 97;
+        int totalmarks , average;
+        total marks = sanskrit + maths + physics;
+        average = totalmarks / 3;
+        System.out.println(totalmarks);
+        System.out.println(average);
+    }
+}
